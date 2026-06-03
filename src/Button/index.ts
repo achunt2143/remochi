79e87dc66195761353a8ce34f1e033902db616e9
achunt2143 <43080643+achunt2143@users.noreply.github.com>
2026-06-02 22:41:09 -0400

    fix: use named export for MochiButton in Button barrel

--- src/Button/index.ts
+++ src/Button/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from './Button';
+export { MochiButton as Button } from './MochiButton';
 export { MochiRadio as Radio } from './MochiRadio';
 export { default as ViewSelectButton } from './ViewSelectButton';
